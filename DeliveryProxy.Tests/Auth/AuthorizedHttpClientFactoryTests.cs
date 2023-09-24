3f1077cd06183130d7f5d35753b2aeab3d3e0f18
zar4za <zar4z4@yandex.ru>
2023-09-24 17:12:17 +0300

    test: initialize project and test authorized http client

---
+++ DeliveryProxy.Tests/Auth/AuthorizedHttpClientFactoryTests.cs
@@ -0,0 +1,23 @@
+﻿using System.Net;
+using DeliveryProxy.Auth;
+using Microsoft.Extensions.Options;
+
+namespace DeliveryProxy.Tests.Auth;
+
+public class AuthorizedHttpClientFactoryTests
+{
+    [Fact]
+    public async Task CreateFactory_CorrectCreds_ShouldAuthorizeApi()
+    {
+        // sample credentials no need to worry about safety
+        var storedConfig = new CdekAuthOptions("EMscd6r9JnFiQ3bLoyjJY6eM78JrJceI", "PjLZkKBHEiLK3YsjtNrt3TGNG0ahs3kG");
+        var options = Options.Create(storedConfig);
+        var factory = new AuthorizedHttpClientFactory(options);
+
+        var httpClient = factory.CreateClient("cdek");
+
+        var result = await httpClient.GetAsync("https://api.edu.cdek.ru/v2/location/postalcodes");
+
+        Assert.NotEqual(HttpStatusCode.Unauthorized, result.StatusCode);
+    }
+}
